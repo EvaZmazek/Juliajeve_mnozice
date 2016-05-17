d46b44dd0085005875d18021bfd0672bb6b593ff
EvaZmazek <eva.zmazek@student.fmf.uni-lj.si>
2016-05-17 21:09:08 +0200

    ............

--- julia/src/algoritmi/Slika.java
+++ julia/src/algoritmi/Slika.java
@@ -1,14 +1,18 @@
 package algoritmi;
 
-import java.awt.Dimension;
+import gui.Platno;
 
 public class Slika {
+	Platno platno;
 	
 	public Double razdalja_na_kvadrat(Complex x, Complex y){
 		Double a = x.re() - y.re();
 		Double b = x.im() - y.im();
 		return Math.abs(a)+Math.abs(b);
-	}
-
-
+		}
+	
+	public void pretvori(int x, int y){
+		double a = platno.getWidth();
+		System.out.println(a);
+		}
 }
--- julia/src/gui/Platno.java
+++ julia/src/gui/Platno.java
@@ -2,18 +2,28 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
+import algoritmi.Slika;
+
 @SuppressWarnings("serial")
-public class Platno extends JPanel {
+public class Platno extends JPanel implements MouseMotionListener {
 	
 	private Okno okno;
+	Slika slika;
+	int sirina = 100;
+	double x=this.getX();
+	double y= this.getY();
 	
 	public Platno(Okno glavno){
 		super();
 		this.setBackground(Color.gray);
+		this.addMouseMotionListener(this); //da sprobavam koordinate
 		this.okno=glavno;
+//		this.setLayout(null);
 	}
 	
 	public Dimension getPreferredSize(){
@@ -29,6 +39,19 @@ public class Platno extends JPanel {
 		double a = Math.max(300, Math.min(x, y));
 		double b = Math.max(300, Math.min(width, height));
 		double c = Math.min(a, b);
+		sirina = (int) c;
 		return new Dimension((int) c, (int)c); //hydra.dimension.height));
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		//TODO
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		System.out.println(e.getX());
+		System.out.println(e.getY());
+		
 	}
 }
